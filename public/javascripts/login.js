d06df9995bf38255ce585994f26de763c248fd89
Conor Igoe <cvigoe@gmail.com>
2016-03-31 09:24:44 -0700

    Added wait message

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -5,8 +5,9 @@ $(document).ready(function(){
     var email = $("input[name='email']").val();
     var password = $("input[name='password']").val();
     $('#login').hide();
-    $('#login').hide();
+    $("body").append("<h1 id='plead'>Bear with me for about 20 seconds...<h1>");
     $.post("/index", {email: email, password: password}, function( data ) {
+      $("#plead").hide();
       if (data === false){
         $("#flash").html("<h3>Please check your username and password</h3>");
         $("#login").show();
