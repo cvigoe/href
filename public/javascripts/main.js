729d81471c3f6bd8ad5cabfa7a42f2985ae73526
Conor Igoe <cvigoe@gmail.com>
2016-04-06 22:01:05 -0700

    Updated Full Thread view

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,16 +6,21 @@ $("a").click(function (event){
 			for(var message in data){
 				if(data[message].body && (data[message].body).indexOf("http") >= 0){
 					if(data[message].senderName == "Conor Igoe"){
-						text = "<div id='me'>" + data[message].body + "</div>";
+						text = ("<a href='" + data[message].body + "'><div id='me'>" + data[message].body + "</div></a>");
 						$("#fullthread").append(text);
 					}
 					else{
-						text = "<div id='you'>" + data[message].body + "</div>";
+						text = "<a href='" + data[message].body + "'><div id='you'>" + data[message].body + "</div></a>";
 						$("#fullthread").append(text);
 					}
 					$("#fullthread").append("<br/>");
 				}
 			}
+
+			$("#fullthread a").each(function (integer, element){
+				$(this).children("div").append("<h1>" + element.hostname + "</h1>");
+			});
+
 			$("#fullthread").append("<button id='back'>Go Back</button>");
 			$("#back").click(function (event){
 				$("#fullthread").empty();
